1670a9cd6a1063c1d555387adc0ff06bc4ece99c
Bogdanchik88 <Bogdanchebotarev@gmail.com>
2026-06-17 09:18:03 +0300

    Updating workflow file for fixture

--- fixtures/base.ts
+++ fixtures/base.ts
@@ -1,9 +1,10 @@
-import { test as base } from "@playwright/test"
+import { test as base, Page } from "@playwright/test"
 import { LoginPage } from "../pages/LoginPage"
 
 
 type Fixtures = {
     loginPage: LoginPage
+    StandardUserPage: Page
 }
 
 export const test = base.extend<Fixtures>({
@@ -11,6 +12,13 @@ export const test = base.extend<Fixtures>({
         const loginPage = new LoginPage(page)
         await loginPage.goto()
         await use(loginPage)
+    },
+
+    StandardUserPage: async({ page }, use) => {
+        const StandardUserPage = new LoginPage(page)
+        await StandardUserPage.goto()
+        await StandardUserPage.login(process.env.STANDARD_USER!, process.env.PASSWORD!)
+        await use(page)
     }
 })
 
